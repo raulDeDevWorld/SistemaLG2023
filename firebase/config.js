dfd8417905e425a90d2ec7f590f84095c9247406
Your Name <you@example.com>
2023-04-28 04:03:01 +0200

    DC

---
+++ firebase/config.js
@@ -0,0 +1,27 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVnLji3yKiQgNZJNR9HWG_GLWPr-dFvvw",
+  authDomain: "logisticsgear-62f44.firebaseapp.com",
+  databaseURL: "https://logisticsgear-62f44-default-rtdb.firebaseio.com",
+  projectId: "logisticsgear-62f44",
+  storageBucket: "logisticsgear-62f44.appspot.com",
+  messagingSenderId: "182577358533",
+  appId: "1:182577358533:web:3321853f8af6a6f0dbb2ac",
+  measurementId: "G-F10P97MQR8"
+};
+  
+
+
+// const firebaseConfig = {
+
+//   apiKey: "AIzaSyA_tsI_bY7j72LAoLwymq2EaeUn2iKD8FQ",
+//   authDomain: "sistema-web-d4dde.firebaseapp.com",
+//   databaseURL: "https://sistema-web-d4dde-default-rtdb.firebaseio.com",
+//   projectId: "sistema-web-d4dde",
+//   storageBucket: "sistema-web-d4dde.appspot.com",
+//   messagingSenderId: "458796101330",
+//   appId: "1:458796101330:web:cec67ad33853d8801f870b"
+  
+//   };
+
+
+  
